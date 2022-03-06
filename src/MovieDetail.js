d7c9f7694d0a3329f5b0a6bd3c68a40854cf13f5
omkargavhane11 <ogomkargavhane@gmail.com>
2022-03-06 17:58:39 +0530

    movie form added

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -2,13 +2,14 @@ import { Counter } from "./Counter";
 import {useState} from "react";
 
 export function MovieDetail ({ url, name, rating,summary }) {
-  const [show,setShow] = useState(false);
+  const [show,setShow] = useState(true);
   const ratingstyles={
     color: rating > 7.9 ? "green":"red",
   };
-  const paraStyle={
-    display:"hidden"
-  };
+  const para = {
+    display:show ? "block":"none",
+  }
+  
   return (
     <div className="maindiv">
       <img src={url} alt="No image" />
@@ -17,8 +18,8 @@ export function MovieDetail ({ url, name, rating,summary }) {
         <h3>{name}</h3>
         <h3 style={ratingstyles}>⭐{rating}</h3>
         </div>
-        {/* <button onClick={() => useState(show)}>Toggle</button> */}
-        <p style={paraStyle}>{summary}</p>
+        <button onClick={()=>setShow(!show)}>Toggle</button>
+        <p style={para}>{summary}</p>
         <Counter />
       </div>
     </div>
